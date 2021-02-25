98620eef10dbbe6dfeb9e591be580274e90b0041
Hunam <elietattevindrevet@gmail.com>
2021-02-25 23:18:39 +0100

    fix(game): defeats optimized

--- src/game.ts
+++ src/game.ts
@@ -38,8 +38,7 @@ export class Game {
     this.points = parseInt(new Function('selector', 'plus', 'always', 'return (selector[2].children[always]' + plus + '.textContent.replace(/ /g,""))')(selector, plus, always)) || 0
     this.games = parseInt(new Function('selector', 'plus', 'always', 'return (selector[3].children[always]' + plus + '.textContent.replace(/ /g,""))')(selector, plus, always)) || 0
     this.victories = parseInt(new Function('selector', 'plus', 'always', 'return (selector[4].children[always]' + plus + '.textContent.replace(/ /g,""))')(selector, plus, always)) || 0
-    if (top1 === 0) this.defeats = parseInt(new Function('selector', 'plus', 'always', 'return (selector[5].children[always]' + plus + '.textContent.replace(/ /g,""))')(selector, plus, always)) || 0
-    else this.defeats = this.games - this.victories
+    this.defeats = this.games - this.victories
     if (new Function('selector', 'plus', 'always', 'top1', 'return (selector[6 - top1].children[always]' + plus + '.textContent.trim().split(" ")[1])')(selector, plus, always, top1) !== undefined) this.playTime = parseInt(new Function('selector', 'plus', 'always', 'top1', 'return (selector[6 - top1].children[always]' + plus + '.textContent.trim().split(" ")[0].slice(0, -1))')(selector, plus, always, top1)) * 60 + parseInt(new Function('selector', 'plus', 'always', 'top1', 'return (selector[6 - top1].children[always]' + plus + '.textContent.trim().split(" ")[1].slice(0, -1))')(selector, plus, always, top1))
     this.kills = parseInt(new Function('selector', 'plus', 'always', 'top1', 'return (selector[7 - top1].children[always]' + plus + '.textContent.replace(/ /g, ""))')(selector, plus, always, top1)) || 0
     this.deaths = parseInt(new Function('selector', 'plus', 'always', 'top1', 'return (selector[8 - top1].children[always]' + plus + '.textContent.replace(/ /g, ""))')(selector, plus, always, top1)) || 0
